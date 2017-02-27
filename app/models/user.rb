8c7eae54cd9ec7e11a0104303364dc2ddfd07a42
panyujin <panyujinyx@gmail.com>
2017-02-27 15:37:19 +0800

    use simple_format helper to decorate

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :posts
 
   has_many :group_relationships
   has_many :participated_groups, :through => :group_relationships, :source => :group
@@ -17,5 +18,5 @@ class User < ApplicationRecord
   def quit!(group)
     participated_groups.delete(group)
   end
-  
+
 end
